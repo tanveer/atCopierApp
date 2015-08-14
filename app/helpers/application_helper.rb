76edb55b96a14be52956e4782e2019f301f781ff
tmbashir <ifarting@gmail.com>
2015-08-13 21:06:09 -0400

    meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def welcome_string
+    if user_signed_in?
+      f_name = current_user.first_name
+      l_name = current_user.last_name
+      return "Welcome #{f_name} #{l_name}"
+    end
+  end
 end
