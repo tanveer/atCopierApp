a3f52ef9a0210f91e4f72cfb45015b719f3b9eff
tmbashir <ifarting@gmail.com>
2015-08-19 07:24:57 -0400

    pagination top and bottom control

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,7 +1,7 @@
 class CodesController < ApplicationController
   
   def index
-    @codes = Code.where(model_id: params[:format]).paginate(:page => params[:page], :per_page => 8).order('created_at ASC')
+    @codes = Code.where(model_id: params[:format]).paginate(:page => params[:page], :per_page => 15).order('created_at ASC')
   end
 
   def main
